e22fa4d0ce17317b074804363c3eec61a6041335
Gagandeep Basra <Basra@gagandeeps-macbook-air.local>
2016-02-02 23:02:19 -0500

    Initial Commit

---
+++ Weather-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by Gagandeep Basra on 2016-02-02.
+//  Copyright © 2016 Gagandeep Basra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
